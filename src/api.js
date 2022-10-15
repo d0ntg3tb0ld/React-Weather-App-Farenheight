6fee1aad64b1d682904e1c413500e6762ab3b81d
d0ng3tb0ld <d0ng3tb0ld@proton.me>
2022-10-15 16:51:02 -0400

    Weather-app-farenheight-buildv1

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: "GET",
+	headers: {
+		'X-RapidAPI-Key': '4d1db1982bmsh988a5095b96729cp1e8d69jsn3404215009d3',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "5b0e133389b39159341d40ddf5ff819d";
